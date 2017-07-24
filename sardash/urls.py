906442627e63119251015d89b0eb263cb27897f8
junshoong <junshoong@gmail.com>
2017-07-24 15:34:10 +0900

    add main page with get_sar_cpu json url

--- sardash/urls.py
+++ sardash/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from main.views import *
+from sardash.views import HomeView
 
 urlpatterns = [
+    url(r'^$', HomeView.as_view(), name='home'),
     url(r'^admin/', admin.site.urls),
     url(r'^cpu/$', get_sar_cpu, name="get_sar_cpu"),
 ]
