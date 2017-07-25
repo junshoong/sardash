366727a2cc8df83c79503ce59dbde5657adec443
junshoong <junshoong@gmail.com>
2017-07-25 14:49:20 +0900

    add server model

--- main/models.py
+++ main/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class Server(models.Model):
+    ip = models.GenericIPAddressField()
+    hostname = models.CharField(max_length=30, blank=True, null=True)
