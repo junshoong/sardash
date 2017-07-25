8f59b27361f5287ef5b24fb173b0d2b0813fa6b4
junshoong <junshoong@gmail.com>
2017-07-25 14:29:25 +0900

    add search address

--- sardash/views.py
+++ sardash/views.py
@@ -1,5 +1,22 @@
-from django.views.generic.base import TemplateView
+from django.views.generic import FormView
+from django.urls import reverse
+
+from main.forms import IPForm
 
 
-class HomeView(TemplateView):
+class HomeView(FormView):
     template_name = 'main.html'
+    form_class = IPForm
+
+    def get_context_data(self, **kwargs):
+        context = super(HomeView, self).get_context_data(**kwargs)
+        ip = self.request.get_full_path()[1:-1]
+        context['ip'] = ip
+        return context
+
+    def form_valid(self, form):
+        return super(HomeView, self).form_valid(form)
+
+    def get_success_url(self):
+        return reverse('home', args=[self.request.POST['address'],])
+
